cab7045ea42fb6eb7bc8c5f698a9aa59cf7fcfb4
Eddie Allard <eddie@theallardcompany.com>
2019-10-09 22:22:58 -0400

    updated apiroutes page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,40 @@
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res){
-        return res.json(friends)
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
 
-    })
-    app.post("/api/friends", function(req, res) {
-        //send all information here and do calculations
-        var userData = req.body;
-        var points = userData.points;
-        totalScore = [];
-    
-        // console.log(userData.points);
-    
-        for (let i = 0; i < points.length; i++) {
-            totalScore.push(parseInt(points[i]));
-        }
-    
-        // console.log(totalScore);
-        for (let j = 0; j < friends.length; j++) {
-            //console.log(friends[j].name, friends[j].points)
-            var totalDifference = totalScore.map(
-                function(a, i) {
-                     return Math.abs(a - parseInt(friends[j].points));
-                    });
-            console.log(`${friends[j].name}'s total diff from you is ${totalDifference.reduce((a,b) => a + b, 0)}`);
-        }
-    })};
+    app.post('/api/friends', function (req, res) {
+        var difference = 100;
+        var matchName = '';
+        var matchPhoto = '';
+
+        friends.forEach(function (friend) {
+            var matchedPointsArray = [];
+            var totalDifference = 100;
+
+            function add(total, num) {
+                return total + num;
+            }
+
+            for (var i = 0; i < friend.points.length; i++) {
+                matchedPointsArray.push(Math.abs(parseInt(req.body.points[i]) - parseInt(friend.points[i])));
+            }
+            totalDifference = matchedPointsArray.reduce(add, 0);
+
+            if (totalDifference < difference) {
+                difference = totalDifference;
+                matchName = friend.name;
+                matchPhoto = friend.photo
+            }
+        });
+
+        res.json({
+            name: matchName,
+            photo: matchPhoto
+        });
+
+        friends.push(req.body);
+    });
+}
